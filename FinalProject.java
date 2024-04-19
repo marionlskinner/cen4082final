ab09380eef5cc9edb0421bb20407d6def5ee2971
Marion Skinner <marionlskinner@gmail.com>
2024-04-18 21:44:53 -0400

    Add files via upload

---
+++ FinalProject.java
@@ -0,0 +1,17 @@
+package cen4802;
+
+import java.util.Random;
+
+public class FinalProject {
+
+	public static void main(String[] args) {
+		
+		Random rand = new Random();
+		int x = rand.nextInt(20) + 1;
+		
+		System.out.println("You rolled a: " + x);
+		
+
+	}
+
+}
